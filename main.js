2dd8a513e4024f5444465d4b297e8ad916de73cb
brian727 <brian.w.727@gmail.com>
2021-07-07 13:59:23 -0700

    styling, js fixed

--- main.js
+++ main.js
@@ -42,9 +42,9 @@ window.onload = function () {
     // Select Catagory
     var selectCat = function () {
       if (chosenCategory === categories[0]) {
-        catagoryName.innerHTML = "The Chosen Category Is Premier League Football Teams";
+        catagoryName.innerHTML = "The Chosen Category Is Comida de Mexicana";
       } else if (chosenCategory === categories[1]) {
-        catagoryName.innerHTML = "The Chosen Category Is Films";
+        catagoryName.innerHTML = "The Chosen Category Is Ocean Creatures";
       } else if (chosenCategory === categories[2]) {
         catagoryName.innerHTML = "The Chosen Category Is Cities";
       }
@@ -183,9 +183,9 @@ window.onload = function () {
     // Play
     play = function () {
       categories = [
-          ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
-          ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
-          ["manchester", "milan", "madrid", "amsterdam", "prague"]
+          ["mole", "ceviche", "agua chile", "chilaqules", "birria", "polsole", "empanada"],
+          ["blue-whale", "octopus", "bottlenose-dolphin", "great-white-shark", "vampire-squid", "wobbegong"],
+          ["tucson", "san diego", "guaymas", "amsterdam", "austin", "tijuana", "puerto moreles", "gilroy",]
       ];
   
       chosenCategory = categories[Math.floor(Math.random() * categories.length)];
@@ -211,9 +211,9 @@ window.onload = function () {
       hint.onclick = function() {
   
         hints = [
-          ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-          ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-          ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
+          ["can contain chocolate", "fresh from the ocean", "hot water", "use up those older corn tortillas and cook this dish", "stewed goat", "hominy soup", "a breakfast pastry, goes well with coffee"],
+          ["biggest ever", "could be alien", "super smart", "wildly misunderstood", "lives very deep, like the undead", "species of bottom dwelling shark"],
+          ["home of the saguaros (in the US)", "whale's vagina", "sonoran port city", "Netherlands capital", "keep this city weird", "home of the busiest border crossing in the world", "magical fishing town in quintana roo", "garlic capital of the world. founded in part by william hanna"]
       ];
   
       var catagoryIndex = categories.indexOf(chosenCategory);
